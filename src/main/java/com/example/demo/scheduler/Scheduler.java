f6310b3770578137f98de35f1970e36b28ac8f05
Vijay <vijay@Vijays-MacBook-Pro.local>
2018-10-04 14:45:20 +0530

    second commit

--- src/main/java/com/example/demo/scheduler/Scheduler.java
+++ src/main/java/com/example/demo/scheduler/Scheduler.java
@@ -1,29 +1,47 @@
 package com.example.demo.scheduler;
 
-import org.springframework.scheduling.annotation.Scheduled;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 @Component
+
     public class Scheduler {
-        @Scheduled(cron = "*/5 * * * * * ")
-        public void cronJobSch() {
-            System.out.println("Java cron job expression:: " + new Date());
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Scheduler.class);
+
+    @Scheduled(cron = "${scheduling.job.cron}")
+
+    public void cronJobSch() {  //
+
+
+        LOGGER.info("Krishna");
+
+           /* System.out.println("Java cron job expression:: " + new Date());
             System.out.println("#######Mai##########Chal########Gyi######Hu####");
-        }
-    @Scheduled(fixedRate = 1000)
+        */
+    }
+
+    @Scheduled(fixedRateString = "${myRate}")
+    //@Scheduled(fixedRateString ="${console.myRate}", initialDelay=5000)
+
     public void fixedRateSch() {
 
-        System.out.println("Fixed Rate scheduler:: " + new Date() + "Hello");
+        LOGGER.info("It is printed when fixedRateSch method is invoked");
+
+        /*System.out.println("Fixed Rate scheduler:: " + new Date() + "Hello");
         System.out.println("#######Mai##########Chal########Gyi######Hu####");
+    */}
 
-    }
-
-    @Scheduled(fixedDelay = 1000, initialDelay = 3000)
+    @Scheduled(fixedDelayString = "${scheduling.job.fixedDelay}" , initialDelayString = "${scheduling.job.initialDelay}")
     public void fixedDelaySch() {
-        System.out.println("Fixed Delay scheduler:: " + new Date() + "hello ");
+
+        LOGGER.info("It is printed when fixedDelayString method is invoked");
+
+        /*System.out.println("Fixed Delay scheduler:: " + new Date() + "hello ");
         System.out.println("#######Mai##########Chal########Gyi######Hu####");
 
+    */
     }
-    }
+}
+
