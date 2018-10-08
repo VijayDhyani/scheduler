9aa42cf1d3f1749d67460468d47e6b2467ff26b9
Vijay <vijay@Vijays-MacBook-Pro.local>
2018-10-08 10:49:40 +0530

    first commit

--- src/main/java/com/example/demo/scheduler/Scheduler.java
+++ src/main/java/com/example/demo/scheduler/Scheduler.java
@@ -4,31 +4,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-@Component
 
-    public class Scheduler {
+@Component
+public class Scheduler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Scheduler.class);
 
     @Scheduled(cron = "${scheduling.job.cron}")
-
-    public void cronJobSch() {//run after every 5 seconds
+    public void cronJobSch() {
         LOGGER.info("Krishna");
     }
 
-    @Scheduled(fixedRateString = "${myRate}")
-
-    public void fixedRateSch() {//takes 3 sec delay to come in action
-
-        LOGGER.info("It is printed when fixedRateSch method is invoked");
-
+    public String printHello() {
+        LOGGER.debug("vijay");
+        return "This is an debug message";
     }
 
-    @Scheduled(fixedDelayString = "${scheduling.job.fixedDelay}" , initialDelayString = "${scheduling.job.initialDelay}")
-    public void fixedDelaySch() { //takes 5 sec delay to come in action with an initial delay of 5 sec
+    public String printError() {
+        LOGGER.error("this is an error");
+        return "this is an error message";
+    }
 
-        LOGGER.info("It is printed when fixedDelayString method is invoked");
+    @Scheduled(fixedRateString = "${myRate}")
+    public void fixedRateSch() {
 
+        LOGGER.warn("It is printed when fixedRateSch method is invoked");
+    }
+
+    @Scheduled(fixedDelayString = "${scheduling.job.fixedDelay}", initialDelayString = "${scheduling.job.initialDelay}")
+    public void fixedDelaySch() {
+
+        LOGGER.error("It is printed when fixedDelayString method is invoked");
     }
 }
 
